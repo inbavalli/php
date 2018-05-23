411f16038a81f721191bff2558b460f50bd3b9ea
inbavalli <36121123+inbavalli@users.noreply.github.com>
2018-05-23 15:18:51 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$subject=array("tamil"=>"91","english"=>"92","science"=>"97","maths"=>"89","social"=>"96");
+echo"tamil:".$subject["tamil"]."<br>";
+echo"english:".$subject["english"]."<br>";
+echo"maths:".$subject["maths"]."<br>";
+echo"science:".$subject["science"]."<br>";
+echo"social:".$subject["social"]."<br>";
+?>
